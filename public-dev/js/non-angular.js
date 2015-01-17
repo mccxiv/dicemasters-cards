27d1f7eba097b59cf972314f84acf88add5f6404
Andrea Stella <adr.stella@gmail.com>
2015-01-17 10:07:06 -0600

    fixed missing animations in bottom cards

--- public-dev/js/non-angular.js
+++ public-dev/js/non-angular.js
@@ -1,3 +1,13 @@
-$(document).on('update', '.nano', function(event, values){
-	console.debug( values );
-});
+(function()
+{
+	var body = $('body');
+
+	$(document).on('update', '.nano', handleNanoScroll);
+
+	function handleNanoScroll(e, values)
+	{
+		if (values.position) body.addClass('scrolling-search-cards');
+		if (!values.position) body.removeClass('scrolling-search-cards');
+	}
+
+})();
