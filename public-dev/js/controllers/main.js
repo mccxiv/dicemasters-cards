a6ba3c6aaf70d1ac12f1db68bf8a549d50969241
Andrea Stella <adr.stella@gmail.com>
2015-01-17 11:49:18 -0600

    fixed nanoscroller bugs

--- public-dev/js/controllers/main.js
+++ public-dev/js/controllers/main.js
@@ -3,5 +3,18 @@ function mainController($scope)
 	var searching = true;
 
 	$scope.isSearching = function() {return searching;};
-	$scope.setSearching = function(bool) {searching = bool;};
+	$scope.setSearching = function(bool)
+	{
+		searching = bool;
+		fixNanoScroller();
+	};
+
+	/**
+	 * Hack! nanoScroller needs to be told to update
+	 * itself after layout changes :(
+	 */
+	function fixNanoScroller()
+	{
+		setTimeout(function() {angular.element('.nano').nanoScroller();}, 400);
+	}
 }
--- public-dev/js/non-angular.js
+++ public-dev/js/non-angular.js
@@ -1,13 +1,28 @@
+
+/**
+ * Add shadow to the search box area when scrolling down the cards
+ * Remove shadow when scrolled all the way up
+ */
 (function()
 {
 	var body = $('body');
-
 	$(document).on('update', '.nano', handleNanoScroll);
-
 	function handleNanoScroll(e, values)
 	{
 		if (values.position) body.addClass('scrolling-search-cards');
 		if (!values.position) body.removeClass('scrolling-search-cards');
 	}
+})();
 
+/**
+ * Get rid of the tabIndex which causes dotted lines around the whole div
+ * There's no option for it in the nanoScroller config
+ * You can only set the tabIndex to -1 but it still gets focused when you click a card
+ */
+(function()
+{
+	setTimeout(function()
+	{
+		$('.nano-content').removeAttr('tabIndex');
+	}, 500);
 })();
